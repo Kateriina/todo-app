094b50922d744ac5aa47d47422ddc307a4783700
Kateriina <blondinkova@mail.ru>
2023-02-27 19:46:09 +0300

    Add func on task

--- src/TodoList.js
+++ src/TodoList.js
@@ -10,7 +10,8 @@ const TodoList = ({list, remove, done }) => {
                 <ul className="todo-list">
                     {list.map((entry) => (
                         <div className="todo">
-                            <li key={entry.id} className={ !entry.status ? "completed" : "todo"}> {entry.title}</li>
+                            <li key={entry.id} className={ !entry.status ? "completed" : "todo"} onClick={() => {done(entry.id)
+                            }}> {entry.title}</li>
 
                             <button className="delete-button"
                                     onClick={() => {remove(entry.id)
